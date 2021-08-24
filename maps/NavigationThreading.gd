1d50ac67500f998d9e411a3a76ad6b696010cb61
Kervin Prinville <prinvillek@mail.com>
2021-08-23 23:24:54 -0400

    Optmized pathing, redid tileSet

---
+++ maps/NavigationThreading.gd
@@ -0,0 +1,19 @@
+extends Navigation2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+	
+func callPath ( start ):
+	return self.get_simple_path(start[0], start[1],false)
